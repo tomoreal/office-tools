69c0634de4579266e996380e1b4e626d88c551c4
Makoto Tomo <soft@xtomo.com>
2026-04-04 07:37:55 +0900

    daily_update_cache.py （日次更新処理）の結果もメールで受け取るようにした。

--- run_daily_update.sh
+++ run_daily_update.sh
@@ -10,7 +10,7 @@
 cd /virtual/tomo/public_html/xbrl3.xtomo.com
 
 # venv内のPython3で日次更新スクリプトを実行
-/virtual/tomo/public_html/xbrl3.xtomo.com/venv/bin/python3 daily_update_cache.py >> daily_update_cache.log 2>&1
-
+# /virtual/tomo/public_html/xbrl3.xtomo.com/venv/bin/python3 daily_update_cache.py >> daily_update_cache.log 2>&1
+/virtual/tomo/public_html/xbrl3.xtomo.com/venv/bin/python3 daily_update_cache.py 2>&1 | tee -a daily_update_cache.log
 # exit codeをそのまま返す
 exit $?
